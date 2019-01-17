d9cb8124ef5a39d02391bc75c536b68a8a97a7a8
MarionA29 <marionaudren@live.fr>
2019-01-17 22:44:00 +0100

    last ones

--- lib/trader_obscur.rb
+++ lib/trader_obscur.rb
@@ -15,19 +15,7 @@ def crazy_scraper
     end
 
   array = key.zip(value).map{|k, v| {key:k, value:v}}
-  puts array[1]
+
 end
 
 crazy_scraper
-def crazy_scraper
-    key = {}
-    value = {}
-  @page.xpath('//td[2]/a').each do |name|
-    key << name.content
-    end
-  @page.xpath('//a[@class="price"]/@data-usd').each do |price|
-    value << price.content
-    end
-
-  return array = key.zip(value).to_a
-end

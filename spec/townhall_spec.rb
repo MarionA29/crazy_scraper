0d3b455139ff9ccd9b17e2e5dcf0616dbb8b4544
MarionA29 <marionaudren@live.fr>
2019-01-17 19:26:58 +0100

    last modifs

--- spec/townhall_spec.rb
+++ spec/townhall_spec.rb
@@ -2,19 +2,19 @@ require_relative "../lib/townhall_spec.rb"
 
 describe "the perform method" do
   it "should return an array with the townhalls' names and emails" do
-    expect(Hash).not_to be_nil
+    expect(perform).not_to be_nil
   end
 end
 
 
 describe "the perform method" do
   it "should return an array with the townhalls' names and emails" do
-    expect(Hash[0]).not_to be_nil
+    expect(get_townhall_urls).not_to be_nil
   end
 end
 
 describe "the perform method" do
   it "should return an array with the townhalls' names and emails" do
-    expect(["villiers-le-bel"]).not_to be_nil
+    expect(get_townhall_email).not_to be_nil
   end
 end
